1718e6ff67f2ee49f8405d976383c92afdf220c4
markotitel <titelskibreg@gmail.com>
2014-11-06 15:57:01 +0100

    Jenkins build script, detect provisioner changes and apply if there are any.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+VAGRANT_BOX_UP=$(vagrant status |grep running)
+ANSIBLE_YES=$(git log --name-status HEAD^..HEAD | grep provision/ | wc -l)
+VAGRANT_YES=$(git log --name-status HEAD^..HEAD | grep Vagrantfile | wc -l)
+
+if [ $? -eq 1 ]
+then
+    vagrant up
+fi
+
+if [ $VAGRANT_YES -gt 0 ]
+then
+    vagrant reload
+elif [ $ANSIBLE_YES -gt 0 ]
+then
+    vagrant provision
+else
+    echo "No changes to Vagrantfile or provision actions, moving on..."
+fi
